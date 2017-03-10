f1fa3fc74b1b07f7637d54464d497b24c2c18ae0
zhangzhengqian <13521832729@163.com>
2017-03-10 09:08:22 +0800

    first maven

---
+++ first_maven_project/src/main/java/junittest/Thread.java
@@ -0,0 +1,30 @@
+/*package junittest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import test.TestThread;
+
+public class Thread {
+	
+	private static TestThread testThread = new TestThread();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+	@Test
+	public void testStatus() {
+		testThread.status();
+	}
+
+	@Test
+	public void testRun() {
+		for(int i=0; i<5;i++){
+			testThread.run();
+		}
+	}
+
+}
+*/
